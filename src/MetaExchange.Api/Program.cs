4a9f1d80d60040dd88bb1942c7c1cf44bc121795
miloshkostadinovski <milos.kostadinoski97@gmail.com>
2025-02-17 20:54:53 +0100

    Preload order books

--- src/MetaExchange.Api/Program.cs
+++ src/MetaExchange.Api/Program.cs
@@ -1,4 +1,5 @@
 using MetaExchange.Core.Extensions;
+using MetaExchange.Core.Services;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -20,6 +21,10 @@ builder.Services
 builder.Services.AddControllers();
 
 var app = builder.Build();
+
+// Preload order books on startup
+var orderBookService = app.Services.GetRequiredService<IOrderBookService>();
+await ((OrderBookService)orderBookService).GetAllOrderBooks();
 
 if (app.Environment.IsDevelopment())
 {
